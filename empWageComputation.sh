76e750ea75f33fe8edf662d79dbc99c054d01dcd
Noor123 <noormd6400@gmail.com>
2021-08-12 18:49:17 +0530

    Salary calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,9 +4,13 @@ echo "Welcome"
 isPresent=1
 isAbsent=0
 randomCheck=$((RANDOM%2))
+empRatePerHr=20
+workHr=8
 if [ $randomCheck -eq $isPresent ]
 then
+    salary=$(( $empRatePerHr * $workHr ))
     echo "employee is present"
 else
     echo "employee is absent"
 fi
+echo "Salary = $salary"
